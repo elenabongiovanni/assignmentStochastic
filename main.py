f217444b363f98f81ecabbc8c02499e68fbaca09
Gaia Catalano <s295111@studenti.polito.it>
2025-03-28 17:16:47 +0100

    distanza euclidea

---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+from instances import *
+from solvers import *
+import json
+
--- scenarioTree.py
+++ scenarioTree.py
@@ -135,3 +135,12 @@ class ScenarioTree(nx.DiGraph):
         )
         return prob, obs
     
+    def euclideanDistance(self, id1, id2):
+        obs_node1 = self.nodes[id1]['obs']
+        # obs_node1 = np.array(self.nodes[id1]['obs']) nel caso in cui non fosse già un array
+        obs_node2 = self.nodes[id2]['obs']
+        return np.linalg.norm(obs_node1-obs_node2)
+    
+
+    
+
